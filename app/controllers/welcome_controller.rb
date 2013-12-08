d7b45277a3eb0c09fef783cc2ccd1d942082708e
Mashpy <mashpy@mashpy-HP-G42-Notebook-PC.(none)>
2013-12-08 14:31:17 +0600

    i will ask question how can i scrap link of the title

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,14 +5,11 @@ require 'open-uri'
 
 class WelcomeController < ApplicationController 
 
-def index
-    doc = Nokogiri::HTML(open("http://esljobs.herokuapp.com/"))
-    @titlearray = []
-        doc.css(".home_wrapper").each do |s|
-            titlecss = s.css("strong a").text
-    linkcss = s.css("strong a").collect { |link| link['href']}
-    @titlearray << titlecss
-    @titlearray << linkcss
+    def index
+      doc = Nokogiri::HTML(open("http://www.eslemployment.com/"))
+      @titlearray = []
+        doc.css(".jobtitle").each do |titlecss|
+        @titlearray << titlecss.text
+        end
     end
-end
 end
